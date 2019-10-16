3926820fc205ad33e2034f38d5cfcf6f7d5424c1
SuperITMan <admin@superitman.com>
2019-10-16 17:50:21 +0200

    Fixed MusicService.findLyrics() in lyricsfinder-v2-initial

--- 12/lyricsfinder-v2-initial/backend/src/musixmatch/services/music-service.intf.ts
+++ 12/lyricsfinder-v2-initial/backend/src/musixmatch/services/music-service.intf.ts
@@ -4,5 +4,5 @@ import { Observable } from 'rxjs';
 export interface MusicService {
   findArtists(name: string): Observable<Artist[]>;
   findSongs(value: string): Observable<Song[]>;
-  findLyrics(songId: number): Observable<SongLyrics>;
+  findLyrics(songId: string): Observable<SongLyrics>;
 }
