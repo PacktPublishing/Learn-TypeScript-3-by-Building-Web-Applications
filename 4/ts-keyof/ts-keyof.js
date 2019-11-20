4071bab032cb4512786f6a3d56ca7306c4239ca3
gaurav-packt <35489117+gaurav-packt@users.noreply.github.com>
2019-11-20 11:55:58 +0530

    renaming folders as per Packt conventions

--- 4/ts-keyof/ts-keyof.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function displayGameProperty(game, propertyName) {
-    console.log(game[propertyName]);
-}
-var game = { name: "Chess", players: 2 };
-displayGameProperty(game, "name");
-//displayGameProperty(game, "foo"); // error TS2345: Argument of type '"foo"' is not assignable to parameter of type '"name" | "players"'
